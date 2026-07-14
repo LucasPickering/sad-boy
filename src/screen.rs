79f01d7caf0943ac65e4c4622138c544b272bf80
Lucas Pickering <lucas@lucaspickering.me>
2026-07-14 07:21:45 -0400

    Box pixel array for screen

--- src/screen.rs
+++ src/screen.rs
@@ -5,25 +5,44 @@
 use base64::{engine::general_purpose::STANDARD, write::EncoderWriter};
 use std::{
     io::{self, Write},
-    mem,
+    mem, slice,
 };
 
-// Screen width/height
-const WIDTH: u16 = 160;
-const HEIGHT: u16 = 144;
+/// Width of the screen in pixels
+const WIDTH_PIXELS: u16 = 160;
+/// Height of the screen in pixels
+const HEIGHT_PIXELS: u16 = 144;
 /// Total number of pixels in the image
-pub const IMAGE_SIZE: usize = WIDTH as usize * HEIGHT as usize;
+const IMAGE_SIZE: usize = WIDTH_PIXELS as usize * HEIGHT_PIXELS as usize;
+/// Width of the screen in terminal columns
+const WIDTH_TERM: u16 = 80;
 
 /// TODO
 pub struct Screen {
-    pixels: [Pixel; IMAGE_SIZE],
+    pixels: Box<[Pixel; IMAGE_SIZE]>,
 }
 
 impl Screen {
     /// TODO
-    #[expect(clippy::large_types_passed_by_value)] // TODO fix this
-    pub fn new(pixels: [Pixel; IMAGE_SIZE]) -> Self {
+    pub fn new(pixels: Box<[Pixel; IMAGE_SIZE]>) -> Self {
         Self { pixels }
+    }
+
+    /// TODO delete
+    pub fn test() -> Self {
+        let red = Pixel::new(255, 0, 0);
+        let green = Pixel::new(0, 255, 0);
+        let blue = Pixel::new(0, 0, 255);
+        let pixels: Vec<Pixel> = (0..IMAGE_SIZE)
+            .map(|i| match i % 3 {
+                0 => red,
+                1 => green,
+                2 => blue,
+                3.. => unreachable!(),
+            })
+            .collect();
+        let pixels: Box<[Pixel; IMAGE_SIZE]> = pixels.try_into().unwrap();
+        Self::new(pixels)
     }
 
     /// Draw the screen to the terminal
@@ -33,14 +52,26 @@ impl Screen {
 
         // Everything other than the escape code is ASCII
         out.write_all(ESCAPE)?;
-        write!(out, "_Ga=T,f=24,s={WIDTH},v={HEIGHT},c=40,r=10;")?;
+        write!(
+            out,
+            "_Ga=T,f=24,s={WIDTH_PIXELS},v={HEIGHT_PIXELS},c={WIDTH_TERM};"
+        )?;
 
         // Read the pixels as raw bytes and encode as base64
-        // SAFETY: TODO
-        let pixel_bytes: [u8; IMAGE_SIZE * 3] =
-            unsafe { mem::transmute(self.pixels) };
+        debug_assert_eq!(mem::size_of::<Pixel>(), 3, "Pixel must be 3 bytes");
+        let ptr: *const [Pixel] = &raw const *self.pixels;
+        // SAFETY:
+        // - Pointer is always valid because we construct it safely above
+        // - Length is correct because we're casting to BYTES, and it's just the
+        //   number of items * bytes per item
+        let pixel_bytes: &[u8] = unsafe {
+            slice::from_raw_parts(
+                ptr.cast(),
+                self.pixels.len() * mem::size_of::<Pixel>(),
+            )
+        };
         let mut b64_writer = EncoderWriter::new(&mut out, &STANDARD);
-        b64_writer.write_all(&pixel_bytes)?;
+        b64_writer.write_all(pixel_bytes)?;
         drop(b64_writer);
 
         // Finish the escape code
