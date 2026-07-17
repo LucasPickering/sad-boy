72fa3013bc58e7ad5acc4a52f2d42d6d3ab2250c
Lucas Pickering <lucas@lucaspickering.me>
2026-07-17 11:28:57 -0400

    Replace clap with lexopt
    Lexopt has no dependencies, so it speeds up compilation.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ mod screen;
 mod util;
 
 use crate::{emu::GameBoy, screen::Screen};
-use clap::Parser;
 use color_eyre::eyre;
+use lexopt::Arg;
 use std::{env, io, path::PathBuf, str::FromStr};
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::{
@@ -12,22 +12,45 @@ use tracing_subscriber::{
     util::SubscriberInitExt,
 };
 
-/// TODO
-#[derive(Parser)]
-struct Args {
-    /// Path to the ROM file to load
-    rom: PathBuf,
-}
-
 fn main() -> eyre::Result<()> {
     color_eyre::install()?;
     initialize_tracing();
-    let args = Args::parse();
+    let args = Args::parse()?;
 
     let mut screen = Screen::test();
     let mut game_boy = GameBoy::boot(&args.rom)?;
     game_boy.run(&mut screen);
     Ok(())
+}
+
+/// CLI args
+struct Args {
+    /// Path to the ROM file to load
+    rom: PathBuf,
+}
+
+impl Args {
+    fn parse() -> Result<Self, lexopt::Error> {
+        // lexopt is a little clunk but it's much lighter than clap
+        let mut rom: Option<PathBuf> = None;
+        let mut parser = lexopt::Parser::from_env();
+        while let Some(arg) = parser.next()? {
+            match arg {
+                Arg::Value(value) if rom.is_none() => {
+                    rom = Some(PathBuf::from(value));
+                }
+                Arg::Long("help") | Arg::Short('n') => {
+                    println!("Usage: sad-boy ROM");
+                    std::process::exit(0);
+                }
+                _ => return Err(arg.unexpected()),
+            }
+        }
+
+        Ok(Self {
+            rom: rom.ok_or("missing argument ROM")?,
+        })
+    }
 }
 
 /// Set up tracing to stderr
