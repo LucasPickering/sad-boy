38c7ded42c569729bbad217ebb0640a98c5ec55e
Lucas Pickering <lucas@lucaspickering.me>
2026-07-24 14:43:35 -0700

    Use shared memory for terminal graphics
    Writing to shared memory should be faster that stdout. I haven't measured it though.

--- src/screen.rs
+++ src/screen.rs
@@ -1,16 +1,30 @@
 //! Graphics bindings for the terminal
 
 use base64::{engine::general_purpose::STANDARD, write::EncoderWriter};
-use std::{
-    io::{self, Stdout, Write},
-    mem, slice,
+use nix::{
+    fcntl::OFlag,
+    libc,
+    sys::{
+        mman::{MapFlags, ProtFlags},
+        stat::Mode,
+    },
 };
-use termion::screen::{AlternateScreen, IntoAlternateScreen};
+use std::{
+    ffi::c_void,
+    io::{self, Stdout, Write},
+    mem,
+    num::NonZero,
+    slice,
+};
+use termion::{
+    cursor,
+    screen::{AlternateScreen, IntoAlternateScreen},
+};
 
 /// Width of the screen in terminal columns
 const WIDTH_TERM: u16 = 80;
 /// TODO
-const IMAGE_ID: ImageId = ImageId(99);
+const SHM_NAME: &str = "/sad_boy_shm";
 /// Terminal escape code to trigger graphics rendering
 ///
 /// https://sw.kovidgoyal.net/kitty/graphics-protocol/#the-graphics-escape-code
@@ -27,14 +41,10 @@ const ESCAPE: &str = "\u{1b}";
 /// the unmodified pixels in the new frame.
 pub struct Screen {
     out: ScreenOut,
-    /// The frame most recently written to the screen
-    ///
-    /// Invariant: `len() == self.width * self.height`
-    pixels_last: Box<[Color]>,
     /// The next frame to write to the screen
     ///
     /// Invariant: `len() == self.width * self.height`
-    pixels_next: Box<[Color]>,
+    pixels: Box<[Color]>,
     width: u16,
     height: u16,
 }
@@ -43,11 +53,10 @@ impl Screen {
     /// Initialize a new screen adapter with the given pixel dimensions
     pub fn new(width: u16, height: u16) -> Self {
         let len = (width * height) as usize;
+        let out = ScreenOut(io::stdout().into_alternate_screen().unwrap());
         Self {
-            out: ScreenOut::new(),
-            // TODO explain
-            pixels_last: vec![Color::WHITE; len].into_boxed_slice(),
-            pixels_next: vec![Color::BLACK; len].into_boxed_slice(),
+            out,
+            pixels: vec![Color::BLACK; len].into_boxed_slice(),
             width,
             height,
         }
@@ -68,7 +77,7 @@ impl Screen {
             height = self.height
         );
         let index = (y * self.width + x) as usize;
-        self.pixels_next[index] = color;
+        self.pixels[index] = color;
     }
 
     /// Draw the current screen buffer to the terminal
@@ -76,59 +85,46 @@ impl Screen {
     /// This will diff the current frame against the last frame. Any differences
     /// will be written out to the terminal.
     pub fn draw(&mut self) {
-        // Diff the previous and next frames, building a series of contiguous
-        // pixels that have changed
-        // TODO join blocks
-        let mut blocks = vec![];
-        for (i, (old, new)) in
-            self.pixels_last.iter().zip(&self.pixels_next).enumerate()
-        {
-            if old != new {
-                let x = i as u16 % self.width;
-                let y = i as u16 / self.width;
-                blocks.push(PixelBlock {
-                    x,
-                    y,
-                    pixels: &self.pixels_next[i..=i],
-                });
-            }
-        }
+        let pixels = &*self.pixels;
+        self.out.set_cursor(1, 1);
 
-        // TODO draw as we go in the diff
-        self.out
-            .write_message(Message::CreateImage { image: IMAGE_ID })
-            .expect("TODO");
-        for block in blocks {
-            self.out
-                .write_message(Message::Frame {
-                    image: IMAGE_ID,
-                    x: block.x,
-                    y: block.y,
-                    pixels: block.pixels,
-                })
-                .expect("TODO");
+        // TODO
+        let len = mem::size_of_val(pixels);
+        nix::sys::mman::shm_unlink(SHM_NAME);
+        let fd = nix::sys::mman::shm_open(
+            SHM_NAME,
+            OFlag::O_RDWR | OFlag::O_CREAT | OFlag::O_EXCL,
+            Mode::S_IRUSR | Mode::S_IWUSR,
+        )
+        .unwrap(); // TODO
+        nix::unistd::ftruncate(&fd, len as i64).unwrap();
+        unsafe {
+            let addr = nix::sys::mman::mmap(
+                None,
+                NonZero::new(len).unwrap(),
+                ProtFlags::PROT_READ | ProtFlags::PROT_WRITE,
+                MapFlags::MAP_SHARED,
+                fd,
+                0,
+            )
+            .expect("mmap");
+            libc::memcpy(addr.as_ptr(), pixels.as_ptr().cast::<c_void>(), len);
         }
         self.out
-            .write_message(Message::Put { image: IMAGE_ID })
+            .write_message(Message::Any {
+                control: &[
+                    Control::Action(Action::TransmitDisplay),
+                    Control::Format(Format::Rgb),
+                    Control::Width(self.width),
+                    Control::Height(self.height),
+                    Control::Columns(WIDTH_TERM),
+                    Control::CursorMovement(false),
+                    Control::Any('t', "s".into()),
+                    Control::Any('S', len.to_string()),
+                ],
+                payload: SHM_NAME.as_bytes(),
+            })
             .expect("TODO");
-
-        /* // Create the image
-        let result = Message::DrawImage {
-            width: self.width,
-            height: self.height,
-            columns: WIDTH_TERM,
-            pixels: &self.pixels_next,
-        }
-        .write(&mut self.out);
-        if let Err(error) = result {
-            error!(%error, "Error drawing to screen");
-        } */
-
-        // Move this frame to the back buffer
-        mem::swap(&mut self.pixels_last, &mut self.pixels_next);
-        // Reset to black. This may not be necessary because the GPU should
-        // rewrite every pixel on every frame, so it could be optimized out
-        self.pixels_next.fill(Color::default());
     }
 }
 
@@ -143,7 +139,6 @@ pub struct Color {
 
 impl Color {
     const BLACK: Self = Self::new(0, 0, 0);
-    const WHITE: Self = Self::new(255, 255, 255);
 
     pub const fn new(red: u8, green: u8, blue: u8) -> Self {
         Self { red, green, blue }
@@ -154,8 +149,10 @@ impl Color {
 struct ScreenOut(AlternateScreen<Stdout>);
 
 impl ScreenOut {
-    fn new() -> Self {
-        Self(io::stdout().into_alternate_screen().unwrap())
+    /// Move the terminal cursor to the **1-based** location, starting from the
+    /// top-left
+    fn set_cursor(&mut self, x: u16, y: u16) -> io::Result<()> {
+        write!(self.0, "{}", cursor::Goto(x, y))
     }
 
     /// Write a graphics message to the output buffer
@@ -201,6 +198,10 @@ impl ScreenOut {
                     Control::ImageId(image_id),
                 ])?;
             }
+            Message::Any { control, payload } => {
+                self.write_control(control)?;
+                self.write_payload(payload)?;
+            }
         }
         write!(self.0, "{ESCAPE}\\")?;
         Ok(())
@@ -219,34 +220,21 @@ impl ScreenOut {
     }
 
     /// Write a series of 24-bit colors as a base64 payload
-    fn write_pixels(&mut self, pixels: &[Color]) -> Result<(), io::Error> {
+    fn write_pixels(&mut self, pixels: &[Color]) -> io::Result<()> {
         let ptr: *const [Color] = &raw const *pixels;
         let pixel_bytes: &[u8] = unsafe {
             slice::from_raw_parts(ptr.cast(), mem::size_of_val(pixels))
         };
-        let mut b64_writer = EncoderWriter::new(&mut self.0, &STANDARD);
-        b64_writer.write_all(pixel_bytes)?;
-        drop(b64_writer);
+        self.write_payload(pixel_bytes)?;
         Ok(())
     }
-}
 
-/// A segment of pixels to be transmitted to the screen
-///
-/// This is generated by diffing two frames. Each block is a contiguous series
-/// of pixels that have changed. Each block will be written to the terminal in
-/// a separate message.
-#[derive(Debug)]
-struct PixelBlock<'a> {
-    /// x coordinate of the first pixel in the block
-    x: u16,
-    /// y coordinate of the first pixel in the block
-    y: u16,
-    /// Contiguous pixels to write
-    ///
-    /// This can overflow the current line, wrapping to the next line at the
-    /// screen width boundary.
-    pixels: &'a [Color],
+    /// Write a base64-encoded binary payload
+    fn write_payload(&mut self, payload: &[u8]) -> io::Result<()> {
+        let mut b64_writer = EncoderWriter::new(&mut self.0, &STANDARD);
+        b64_writer.write_all(payload)?;
+        Ok(())
+    }
 }
 
 /// A message for the terminal in the [Terminal Graphics Protocol](https://sw.kovidgoyal.net/kitty/graphics-protocol/#control-data-reference)
@@ -268,12 +256,17 @@ enum Message<'a> {
     },
     /// Display a previously transmitted image to the screen
     Put { image: ImageId },
+    /// TODO
+    Any {
+        control: &'a [Control],
+        payload: &'a [u8],
+    },
 }
 
 /// A single control attribute in a graphics protocol message
 ///
 /// https://sw.kovidgoyal.net/kitty/graphics-protocol/#control-data-reference
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 enum Control {
     /// `a`: What action should the terminal take?
     Action(Action),
@@ -293,37 +286,41 @@ enum Control {
     Columns(u16),
     /// `C`: Enable/disable cursor movement after placing the image
     CursorMovement(bool),
+    /// TODO
+    Any(char, String),
 }
 
 impl Control {
-    fn key(self) -> char {
+    fn key(&self) -> char {
         match self {
-            Control::Action(_) => 'a',
-            Control::Format(_) => 'f',
-            Control::ImageId(_) => 'i',
-            Control::Width(_) => 's',
-            Control::Height(_) => 'v',
-            Control::X(_) => 'x',
-            Control::Y(_) => 'y',
-            Control::Columns(_) => 'c',
-            Control::CursorMovement(_) => 'C',
+            Self::Action(_) => 'a',
+            Self::Format(_) => 'f',
+            Self::ImageId(_) => 'i',
+            Self::Width(_) => 's',
+            Self::Height(_) => 'v',
+            Self::X(_) => 'x',
+            Self::Y(_) => 'y',
+            Self::Columns(_) => 'c',
+            Self::CursorMovement(_) => 'C',
+            Self::Any(key, _) => *key,
         }
     }
 
-    fn value(self) -> String {
+    fn value(&self) -> String {
         match self {
-            Control::Action(Action::TransmitDisplay) => "T".into(),
-            Control::Action(Action::Frame) => "f".into(),
-            Control::Action(Action::Put) => "p".into(),
-            Control::Format(Format::Rgb) => "24".into(),
-            Control::CursorMovement(false) => "1".into(),
-            Control::CursorMovement(true) => "0".into(),
-            Control::ImageId(id) => id.0.to_string(),
-            Control::Width(v)
-            | Control::Height(v)
-            | Control::X(v)
-            | Control::Y(v)
-            | Control::Columns(v) => v.to_string(),
+            Self::Action(Action::TransmitDisplay) => "T".into(),
+            Self::Action(Action::Frame) => "f".into(),
+            Self::Action(Action::Put) => "p".into(),
+            Self::Format(Format::Rgb) => "24".into(),
+            Self::CursorMovement(false) => "1".into(),
+            Self::CursorMovement(true) => "0".into(),
+            Self::ImageId(id) => id.0.to_string(),
+            Self::Width(v)
+            | Self::Height(v)
+            | Self::X(v)
+            | Self::Y(v)
+            | Self::Columns(v) => v.to_string(),
+            Self::Any(_, v) => v.clone(),
         }
     }
 }
